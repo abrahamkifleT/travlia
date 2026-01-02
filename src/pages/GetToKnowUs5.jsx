fe3afb3d22c8541cd108420a47f51293266f7cdf
abraham <abrahamkifle1212@gmail.com>
2026-01-02 21:55:32 +0300

    implement responsiveness in GetToKnowUs5

--- src/pages/GetToKnowUs5.jsx
+++ src/pages/GetToKnowUs5.jsx
@@ -6,7 +6,7 @@ const GetToKnowUs5 = () => {
     return (
         <div className="flex flex-col items-center my-15">
             <p>GetToKnowUs</p>
-            <div className="flex mx-20 pt-15">
+            <div className="flex flex-col md:flex-row mx-4 md:mx-20 pt-15 gap-8 md:gap-0">
                 <GetToKnowUs3Card firstImage={firstImage} secondImage={secondImage} thirdImage={thirdImage} />
 
                 <GetToKnowUs3Card firstImage={secondImage} secondImage={thirdImage} thirdImage={firstImage} />
